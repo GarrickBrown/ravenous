378940fcd1f0668e4dbe3af05bba31b339706807
Garrick Brown <41980127+GarrickBrown@users.noreply.github.com>
2018-09-22 15:51:51 +0800

    Simulate search query with term, location and sortBy

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -14,6 +14,9 @@ class SearchBar extends React.Component {
 			'Highest Rated': 'rating',
 			'Most Reviewed': 'review_count'
 		};
+		this.handleTermChange = this.handleTermChange.bind(this);
+		this.handleLocationChange = this.handleLocationChange.bind(this);
+		this.handleSearch = this.handleSearch.bind(this);
 	}
 
 	getSortByClass(sortByOption) {
@@ -24,6 +27,23 @@ class SearchBar extends React.Component {
 		this.setState({
 			sortBy: sortByOption
 		});
+	}
+
+	handleTermChange(event) {
+		this.setState({
+			term: event.target.value
+		});
+	}
+
+	handleLocationChange(event) {
+		this.setState({
+			location: event.target.value
+		});
+	}
+
+	handleSearch(event) {
+		this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+		event.preventDefault();
 	}
 
 	renderSortByOptions() {
@@ -42,11 +62,11 @@ class SearchBar extends React.Component {
 					</ul>
 				</div>
   				<div className="SearchBar-fields">
-					<input placeholder="Search Businesses" />
-					<input placeholder="Where?" />
+					<input placeholder="Search Businesses" onChange={this.handleTermChange} />
+					<input placeholder="Where?" onChange={this.handleLocationChange} />
 				</div>
 				<div className="SearchBar-submit">
-					<a>Let's Go</a>
+					<a onClick={this.handleSearch}>Let's Go</a>
 				</div>
 			</div>
 		);
